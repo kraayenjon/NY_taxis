c5aabeb2a2f1b1079fc6aa9e43ae60279de92fab
Jonathan Kraayenbrink <121937557+kraayenjon@users.noreply.github.com>
2023-11-08 14:44:13 +0100

    Update NYapp2.py

--- NYapp2.py
+++ NYapp2.py
@@ -8,12 +8,6 @@ from io import BytesIO
 import zipfile
 import tempfile
 
-col1, col2, col3 = st.columns(3)
-col1.metric("Temperature", "70 °F", "1.2 °F")
-col2.metric("Wind", "9 mph", "-8%")
-col3.metric("Humidity", "86%", "4%")
-
-
 @st.cache(suppress_st_warning=True, allow_output_mutation=True)
 def load_and_process_data():
     final_df = pd.DataFrame()
